7b219fbe00b14e8b2d93201eba767deca692616e
Kevin Christian <kdotchrist30@gmail.com>
2024-06-14 20:04:00 +0700

    feat: added user provider protection

--- src/services/auth/me.get.ts
+++ src/services/auth/me.get.ts
@@ -1,13 +1,19 @@
 import http from "@/lib/axios";
+import makeResponseDTO from "@/lib/response";
 import { z } from "zod";
 
-const meResponseValidation = z.object({
-  sub: z.string()
-});
+const meResponseValidation = makeResponseDTO(
+  z.object({
+    email: z.string(),
+    id: z.string(),
+    name: z.string(),
+    role: z.string()
+  })
+);
 
 export type MeResponse = z.infer<typeof meResponseValidation>;
 
-export const meGet = async () => {
-  // const res = await http.get<MeResponse>("users/me");
-  // return meResponseValidation.parse(res.data);
+export const meGet = async (): Promise<MeResponse> => {
+  const res = await http.get<MeResponse>("user");
+  return meResponseValidation.parse(res.data.data);
 };
