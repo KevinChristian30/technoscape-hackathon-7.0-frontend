4e47fbab5f96d851ee4f260273a54b6095f1df4a
Kevin Christian <kdotchrist30@gmail.com>
2024-06-15 02:41:24 +0700

    scaffolded email

--- src/app/(main)/(agent)/customer-emails/page.tsx
+++ src/app/(main)/(agent)/customer-emails/page.tsx
@@ -1,7 +1,25 @@
+import EmailSection from "@/components/domain/email/EmailSection";
+import {
+  PageLayout,
+  PageLayoutContent,
+  PageLayoutHeader,
+} from "@/components/layouts/PageLayout";
 import React from "react";
 
 const Page = () => {
-  return <div>Customer Emails</div>;
+  return (
+    <PageLayout>
+      <PageLayoutHeader className="flex justify-between items-center w-full">
+        Emails
+      </PageLayoutHeader>
+
+      <div className="h-8"></div>
+
+      <PageLayoutContent className="w-full">
+        <EmailSection />
+      </PageLayoutContent>
+    </PageLayout>
+  );
 };
 
 export default Page;
