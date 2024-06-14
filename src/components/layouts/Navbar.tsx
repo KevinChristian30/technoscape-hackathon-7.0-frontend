18db86d8c9b3299d80f6e70dca1928a24173e424
Josafat Anderson <89123059+lowkeyy420@users.noreply.github.com>
2024-06-14 14:29:17 +0700

    feat: add theme toggle button

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SidePanelMobile from './SidePanelMobile'
 import ProfileButton from '../ui/ProfileButton'
+import ThemeToggleButton from '../ui/ChangeThemeButton'
 
 const Navbar = () => {
     return (
         <>
             <SidePanelMobile />
+            <ThemeToggleButton />
             <ProfileButton />
         </>
     )
