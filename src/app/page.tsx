ae4be09544b462a1a0f23cfaca7b127c3d2062d0
Kevin Christian <kdotchrist30@gmail.com>
2024-06-14 10:53:07 +0700

    feat: added login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,40 @@
+"use client";
+
+import { socket } from "@/socket/socket";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState<any>([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value: any) {
+      setFooEvents([...fooEvents, value]);
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("foo", onFooEvent);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("foo", onFooEvent);
+    };
+  }, []);
+
   return (
-    <main></main>
+    <div className="App">
+      {isConnected ? <p>Connected</p> : <p>Not Connected</p>}
+      <button onClick={() => socket.connect()}>Connect to socket</button>
+    </div>
   );
 }
